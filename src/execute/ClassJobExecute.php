d5ef8fa32cfe0eff3902a49e6a9c50e3e65a7b2c
yuanzhihai <396751927@qq.com>
2023-06-13 11:52:55 +0800

    add class task

---
+++ src/execute/ClassJobExecute.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace easyyuan\crontab\execute;
+
+class ClassJobExecute extends JobExecute
+{
+    /**
+     * run
+     * @param array $data
+     * @return bool
+     */
+    public function run(array $data): bool
+    {
+        $callback = json_decode( $data['command'],true );
+        [$class,$method] = $callback;
+        $parameters = $callback[2] ?? null;
+        if ($class && $method && class_exists( $class ) && method_exists( $class,$method )) {
+            $instance = new $class();
+            if ($parameters && is_array( $parameters )) {
+                $instance->{$method}( ...$parameters );
+            } else {
+                $instance->{$method}();
+            }
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * validate
+     * @param string $command
+     * @return bool
+     */
+    public static function validate(string $command): bool
+    {
+        return true;
+    }
+}
